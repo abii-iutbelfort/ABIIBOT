d29bbc3dc1b0b3967a96e8c67d066308512757a2
chevalme arnaud <arnaud.chevalme@edu.univ-fcomte.fr>
2023-05-24 00:47:17 +0200

    Mets à jour la commande ping qui affiche plus de détail

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,19 +1,35 @@
-const {SlashCommandBuilder} = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 const data = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('Replies with Pong!')
-  .addBooleanOption(option => option.setName('delay')
-    .setDescription('Whether to display your ping'))
+  .setName("ping")
+  .setDescription("Replies with Pong!")
+  .addBooleanOption((option) =>
+    option
+      .setName("visible")
+      .setDescription("Whether to display your ping to everyone !")
+  );
 
 const execute = async (interaction) => {
-  let msg = 'Pong !';
-  if (interaction.options.getBoolean('delay')) msg += ` (${interaction.client.ws.ping}ms)`;
-  await interaction.reply({content: msg, ephemeral: true});
+  const msg = await interaction.reply({
+    content: `Pong !\nWebsocket heartbeat: ${interaction.client.ws.ping}ms.\nPinging...`,
+    ephemeral: !interaction.options.getBoolean("visible"),
+    fetchReply: true,
+  });
+
+  interaction.editReply(
+    msg.content.replace(
+      "Pinging...",
+      `Roundtrip latency: ${
+        msg.createdTimestamp - interaction.createdTimestamp
+      }ms`
+    )
+  );
 };
 
-const help = "Replies with Pong!\n" +
-  "Options :\n" + "`delay` : Whether to display your ping (default false)\n" +
-  "**Usage** : ```/ping [delay]```"
+const help =
+  "Replies with Pong!\n" +
+  "Options :\n" +
+  "`delay` : Whether to display your ping (default false)\n" +
+  "**Usage** : ```/ping [delay]```";
 
-module.exports = {data, execute, help};
+module.exports = { data, execute, help };
